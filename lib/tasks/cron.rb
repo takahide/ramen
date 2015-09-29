05c32b96c2e9805dc8d0815c5282163d4dd7c318
takahide <h@takahi.de>
2015-09-30 01:00:22 +0900

    something

--- lib/tasks/cron.rb
+++ lib/tasks/cron.rb
@@ -68,7 +68,7 @@ class Cron
   end
 
   def self.get_tsukemen_restaurant_info
-    TsukemenRestaurant.find_each do |r|
+    TsukemenRestaurant.where(name: nil).find_each do |r|
       s = RamendbRestaurantScraper.new r.url
       s.html = r.html
 
